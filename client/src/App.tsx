34fbd44b1c0d84ebe67c3e354acf796ddf47381f
Wiktor Trybus <wiktortrybus11@gmail.com>
2026-01-08 21:56:16 +0100

    Initial add product page done, several fixes to be made

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
 import AdminProductDetailsPage from "./pages/AdminProductDetailsPage";
+import AdminProductCreatePage from "./pages/AdminProductCreatePage";
 
 export default function App() {
   return (
@@ -25,6 +26,7 @@ export default function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/admin/products/:id" element={<AdminProductDetailsPage />} />
+        <Route path="/admin/products/new" element={<AdminProductCreatePage />} />
       </Routes>
     </>
   );
